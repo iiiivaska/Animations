bcb1254d65e7cd3257daa666e4c79e25a933688b
iiiivaska <iiiivaska@yandex.ru>
2021-10-21 11:44:19 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Василий Буланов on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
