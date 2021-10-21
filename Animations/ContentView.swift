09f86c1be96fdefaa9867eb605defb529d389735
iiiivaska <iiiivaska@yandex.ru>
2021-10-21 14:26:44 +0300

    tests with custom animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,15 +13,33 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            self.animationAmount += 1
+//            self.animationAmount += 1
         }
         .padding(50)
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default)
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(.easeOut(duration: 1).repeatForever(autoreverses: false))
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+//        //.blur(radius: (animationAmount - 1) * 3)
+//        //.animation(.easeOut)
+//        //.animation(.easeOut(duration: 2).delay(1))
+//        //.animation(.easeOut(duration: 1).repeatCount(3, autoreverses: true))
+//        .animation(.easeOut(duration: 1).repeatForever(autoreverses: true))
+//        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
     }
 }
 
