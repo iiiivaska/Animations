9cd42d9219bfe2642a27e8f09ecda7fc89aa5c86
iiiivaska <iiiivaska@yandex.ru>
2021-10-21 15:00:58 +0300

    Add button animation with Stepper

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -12,34 +12,24 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
     
     var body: some View {
-        Button("Tap Me") {
-//            self.animationAmount += 1
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(Animation.easeInOut(duration: 1)
+                                                                        .repeatCount(3, autoreverses: true)
+                                                                ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(.easeOut(duration: 1).repeatForever(autoreverses: false))
-        )
-        .onAppear {
-            self.animationAmount = 2
-        }
-//        .padding(50)
-//        .background(Color.red)
-//        .foregroundColor(.white)
-//        .clipShape(Circle())
-//        .scaleEffect(animationAmount)
-//        //.blur(radius: (animationAmount - 1) * 3)
-//        //.animation(.easeOut)
-//        //.animation(.easeOut(duration: 2).delay(1))
-//        //.animation(.easeOut(duration: 1).repeatCount(3, autoreverses: true))
-//        .animation(.easeOut(duration: 1).repeatForever(autoreverses: true))
-//        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
     }
 }
 
